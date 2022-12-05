fac0c7627aeb720efa4faa80ff2b465d43881836
wenliangsu <wenliangsu65018@gmail.com>
2022-12-05 12:59:03 +0800

    Feat: add express-session and logout.hbs

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 //Section Setting the variable
 const express = require("express");
 const exphbs = require("express-handlebars");
+
+//note 導入session 
+const session = require("express-session")
+
 const methodOverride = require("method-override");
 
 const routes = require("./routes")
@@ -22,6 +26,17 @@ app.use(express.static("public"));
 
 //body-parser
 app.use(express.urlencoded({ extended: true }));
+
+// session setting
+//note 針對session 以及cookie裡的設定值
+app.use(session({
+  secret: 'This a secret test for Wen', //note 此為require option, 存放在cookie中的hashed session id 
+  resave: false,
+  saveUninitialized: false,
+  // note Maxage設定多久後cookie要消滅掉，null或是不設定則一關browder就會不見，要再重新登入
+  //note httpOnly則是設定不可使用code操作cookie
+  cookie : {maxAge: 60000, httpOnly: true}
+}))
 
 // router pre-treat
 app.use(methodOverride("_method"));
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,23 +4,34 @@ const router = express.Router()
 const userDataBase = require("../../models/userInfo");
 
 //Section route
+//todo keeping the login page or not
 router.get("/", (req, res) => {
+  //note 此設定為若從不同網站過來的時候，可以保持在登入狀態
+  if (req.session.login) {
+    res.render('back', { userName: req.session.name});
+  }
   res.render("index");
 });
 
+//todo Check if already login or not
 router.post("/", (req, res) => {
-  const { email, password } = req.body;
+  const { email, password } = req.body; //解構賦值((Destructuring assignment)
 
   userDataBase
     .findOne({ email, password })
     .lean()
     .then((userInfo) => {
-      if (userInfo) {
-        // Match the user of dataBase
-        res.render("show", { userInfo });
-      } else {
-        // Can't match the user Info from dataBase
+     if (userInfo) {
+       //search and compare the user info
+       //note 在session裡面新增兩個key-value
+       req.session.login = true;
+       req.session.name = userInfo.firstName;
+       // console.log(req.session)
 
+       // Match the user of dataBase
+       res.render("show", { userInfo });
+     } else {
+        // Can't match the user Info from dataBase
         res.render("index", { error: "Email or Password is not wrong" });
       }
     })
@@ -29,5 +40,14 @@ router.post("/", (req, res) => {
       res.render("error", { error: error.message });
     });
 });
+
+// todo logout and destroy the record
+
+router.get('/logout', (req, res) => {
+  req.session.destroy()
+  console.log("Already destroy the session")
+  res.redirect("/")
+})
+
 
 module.exports = router
